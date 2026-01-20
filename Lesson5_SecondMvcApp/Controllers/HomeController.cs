99d02f52d7311b1d442cb6ed7efdd166c69793f0
karimzade_k <karimzade_k@STHQ0118-01>
2026-01-20 19:55:01 +0400

    Add project files.

---
+++ Lesson5_SecondMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson5_SecondMvcApp.Controllers;
+
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Gallary()
+    {
+        return View();
+    }
+
+}
---
+++ Lesson5_SecondMvcApp/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseDeveloperExceptionPage();
+// }
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapControllerRoute(
+        name:"default",
+        pattern:"{controller=Home}/{action=Index}/{id?}");
+});
+
+
+app.Run();
