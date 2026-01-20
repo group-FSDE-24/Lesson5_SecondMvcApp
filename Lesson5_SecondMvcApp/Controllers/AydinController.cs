166f8b07c8194f44d874d8ec205f6a0e33cfc0fa
karimzade_k <karimzade_k@STHQ0118-01>
2026-01-20 20:19:58 +0400

    ViewStart bitdi

---
+++ Lesson5_SecondMvcApp/Controllers/AydinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson5_SecondMvcApp.Controllers
+{
+    public class AydinController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
